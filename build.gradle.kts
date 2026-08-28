6daed718c95877670b29850d3e96b94c8efeb88e
BruderJulian <julian@familie-zech.de>
2026-08-28 22:41:48 +0200

    Migrate build to Gradle and add CI tooling
    - Replace Maven (pom.xml, shade) with Gradle + Shadow fat jar
    - Fix plugin.yml main class to com.lemonlightmc.minecicd.MineCICD
    - Remove IntelliJ (.idea/, *.iml) from version control
    - Add comprehensive .gitignore for Gradle/Java
    - Add GitHub Actions: build shadow jar on push/PR (JDK 17/21 matrix), attach to releases

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("com.gradleup.shadow") version libs.versions.shadow
+}
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.eclipse.jgit)
+    implementation(libs.commons.io)
+    implementation(libs.json)
+    implementation(libs.commons.lang3)
+    compileOnly(libs.paper.api)
+}
+
+group = "com.lemonlightmc"
+version = "2.2.1"
+description = "A CI/CD plugin for Minecraft Servers and Networks"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+    mergeServiceFiles()
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+}
+
+tasks.jar {
+    archiveClassifier.set("thin")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["shadow"])
+    }
+}
+
+tasks.register("printVersion") {
+    doLast {
+        println(version)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
